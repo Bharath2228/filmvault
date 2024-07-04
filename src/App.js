b0c1338783bbcad283fa46d166291262869a29ce
Bharath2228 <pbharath2210@gmail.com>
2024-07-04 22:34:08 +0530

    Update: Header & Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { AllRoutes } from "./routes/AllRoutes"
 import './App.css'
+import { Header, Footer } from "./components"
 
 export const App = () => {
   return (
     <div className="App">
+      <Header />
       <AllRoutes />
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom"
+
 export const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-white shadow dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">FilmVault</Link>. All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+            </li>
+            <li>
+                <a href="#" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+            </li>
+            <li>
+                <a href="#" target="_blank" className="hover:underline me-4 md:me-6">GitHub</a>
+            </li>
+            <li>
+                <a href="#" target="_blank" className="hover:underline">Contact</a>
+            </li>
+        </ul>
+        </div>
+    </footer>
+
   )
 }
