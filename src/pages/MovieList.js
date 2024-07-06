854f45c69299f6f14a9003bf3996eb497884cfde
Bharath2228 <pbharath2210@gmail.com>
2024-07-06 13:14:17 +0530

    Update: All the links of Navbar

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,9 +1,19 @@
-export const MovieList = () => {
+import { useFetch } from "../hooks/useFetch";
+import { Card } from "../components"
+
+export const MovieList = ({apiPath}) => {
+
+  const { data: movies } = useFetch(apiPath)
+
   return (
     <main >
-      <div>
-        MovieList
-      </div>
+      <section className="max-w-7xl mx-auto py-7">
+          <div className="flex justify-start flex-wrap"> 
+            { movies.map((movie) => (
+              <Card key={movie.id} movie ={movie}/>
+            ))}
+          </div>
+      </section>
     </main>
   )
 }
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -3,16 +3,16 @@ import { MovieList, MovieDetail, Search, PageNotFound } from "../pages";
 
 export const AllRoutes = () => {
   return (
-    <>
+    <div className="dark:bg-slate-900">
         <Routes>
-            <Route path="/" element={<MovieList />} />
+            <Route path="/" element={<MovieList apiPath="movie/now_playing"/>} />
             <Route path="movie/:id" element={<MovieDetail />} />
-            <Route path="movies/popular" element={<MovieList />} />
-            <Route path="movies/top" element={<MovieList />} />
-            <Route path="movies/upcoming" element={<MovieList />} />
+            <Route path="movies/popular" element={<MovieList apiPath="movie/popular"/>} />
+            <Route path="movies/top" element={<MovieList apiPath="movie/top_rated" />} />
+            <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" />} />
             <Route path="search" element={<Search />} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
-    </>
+    </div>
   )
 }
